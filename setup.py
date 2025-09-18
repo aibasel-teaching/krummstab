79bac439e3f4f5ecf653a68b19751566a51fb73b
Remo Christen <remo.christen@unibas.ch>
2025-09-18 16:34:55 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="krummstab",
-    version="1.0",
+    version="1.0.1",
     description="Efficiently give feedback on ADAM submissions at University of Basel",
     long_description=long_description,
     long_description_content_type="text/markdown",
